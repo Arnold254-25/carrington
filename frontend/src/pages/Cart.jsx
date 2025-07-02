aea54a5a56f2e753e409119437b4c811c08ca97c
Arnold Tony <arnoldtony269@gmail.com>
2025-07-02 12:08:25 +0300

    second commit

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -1,6 +1,28 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { ShopContext } from '../context/ShopContext'
 
 const Cart = () => {
+
+const {products, currency, cartItems} = useContext(ShopContext);
+
+const [cartData,setCartData] = useState([]);
+
+
+useEffect(()=>{
+
+
+  const tempData = [];
+
+  for (const items in cartItems){
+    for (const item in cartItems[items]){
+
+      
+  }
+
+},[cartItems])
+
+
+
   return (
     <div>Cart</div>
   )
